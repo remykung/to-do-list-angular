752d64652e822753aa956d3036644fe06fd3646a
Yunchuan <leisyun@gmail.com>
2024-02-11 14:29:09 +0800

    arrange layout and set input validation

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -18,11 +18,13 @@ import { MatIconModule } from '@angular/material/icon';
 export class TaskListComponent implements OnInit {
   // make this string list dynamic 
   tasklist: string[] = [];
+  isEmpty: boolean = true;
   constructor(private taskService: TaskService) {}
 
   ngOnInit() {
     this.taskService.currentTasks.subscribe(tasks => {
       this.tasklist = tasks;
+      this.isEmpty = this.tasklist.length === 0; 
     });
  }
  deleteTask(index: number) {
